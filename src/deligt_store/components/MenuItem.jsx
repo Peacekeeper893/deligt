55e8aa551eb0314302eef820c4c8550f1a8d6f26
Navneet Sindhwani <navneetsindhwani009@gmail.com>
2023-06-24 11:36:56 +0530

    new commit

--- src/deligt_store/components/MenuItem.jsx
+++ src/deligt_store/components/MenuItem.jsx
@@ -27,8 +27,8 @@ const MenuItem = (props) => {
       className="py-5 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-zinc-300 via-neutral-200 to-neutral-500 text-black rounded-xl p-6 my-4 flex flex-col md:flex-row justify-between md:gap-16 gap-4">
       <div>
         <p className="md:text-3xl text-xl text-opacity-100 font-bold">{props.name}</p>
-        <p className="my-2 md:text-md text-sm ">{props.description}</p>
-        <p className="font-semibold">{`$${props.price}`}</p>
+        <p className="my-2 md:text-md text-sm max-w-[1000px] ">{props.description}</p>
+        <p className="font-semibold">{`$${props.price.toFixed(2)}`}</p>
       </div>
 
       <div>
